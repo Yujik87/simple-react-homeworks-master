97e295b3f7ba9563f414eeb618cd98a7d2f0d8b5
Yujik <yujika87@gmail.com>
2022-01-07 00:09:02 +0300

    HW3

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -26,10 +26,10 @@ const Greeting: React.FC<GreetingPropsType> = (
                     onKeyDown={onEnter}
                     onBlur={setNameCallback}
                 />
-                <span>{error}</span>
-                <button onClick={addUser}>add</button>
-                <span>{totalUsers}</span>
+                <div className={s.error}>{error}</div>
             </div>
+            <button onClick={addUser} disabled={!name}>add</button>
+            <span>{totalUsers}</span>
         </div>
     )
 }
